f6599fbaa225130efb831eeff0fece008ebd0053
imrahulv <181220046@nitdelhi.ac.in>
2021-04-17 14:10:54 +0530

    updated transactions

--- frontend/src/MakeTransaction/TransactionForm.js
+++ frontend/src/MakeTransaction/TransactionForm.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import { Button, InputGroup } from "react-bootstrap";
-import './MakeTransaction.css';
-
-
+import "./MakeTransaction.css";
 
 class MakeTransaction extends Component {
   constructor(props) {
     super(props);
     this.state = {
       allcustomers: [],
+
       selectFrom: "",
       selectTo: "",
       balance: "",
@@ -21,13 +20,14 @@ class MakeTransaction extends Component {
     fetch("http://localhost:3002/customers")
       .then((res) => res.json())
       .then((result) => {
+        console.log(result);
         this.setState({
           allcustomers: result,
         });
       })
-      .catch(err=>{
-        console.log("failed to fetch customers")
-      })
+      .catch((err) => {
+        console.log("failed to fetch customers");
+      });
   }
 
   handleSelectFromChange = (event) => {
@@ -48,123 +48,129 @@ class MakeTransaction extends Component {
     });
   };
 
-  getData = (rowData) => {
-    console.log(rowData);
-  };
-
   handleSubmit = (event) => {
-    fetch("http://localhost:3002/transaction", {
-      method: "put",
-      headers: { "Content-Type": "application/json" },
+    let senderbalance = 0;
 
-      body: JSON.stringify({
-        sender_name: this.state.selectFrom,
-        bal: this.state.balance,
-        reciever_name: this.state.selectTo,
-      }),
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        if (response) {
-          fetch("http://localhost:3002/transactionHistory", {
-            method: "post",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              sender_name: this.state.selectFrom,
-              bal: this.state.balance,
-              reciever_name: this.state.selectTo,
-            }),
-          })
-            .then((response) => response.json())
-            .then((transactionResult) => {
-              this.setState({
-                transactionHistory: transactionResult,
-              });
-            });
-        }
+    for (const val of this.state.allcustomers) {
+      if (val.name === this.state.selectFrom) {
+        senderbalance = val.amount;
+      }
+    }
+
+    if (senderbalance - this.state.balance < 0) {
+      return alert("Insufficient Balance , Transaction Failed");
+    } else {
+      fetch("http://localhost:3002/transaction", {
+        method: "put",
+        headers: { "Content-Type": "application/json" },
+
+        body: JSON.stringify({
+          sender_name: this.state.selectFrom,
+          bal: this.state.balance,
+          reciever_name: this.state.selectTo,
+        }),
       })
-      .catch((err) => {
-        console.log("failed to make transaction");
-      });
+        .then((response) => response.json())
+        .then((response) => {
+          if (response) {
+            fetch("http://localhost:3002/transactionHistory", {
+              method: "post",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                sender_name: this.state.selectFrom,
+                bal: this.state.balance,
+                reciever_name: this.state.selectTo,
+              }),
+            })
+              .then((response) => response.json())
+              .then((transactionResult) => {
+                this.setState({
+                  transactionHistory: transactionResult,
+                });
+              });
+          }
+        })
+        .catch((err) => {
+          console.log("failed to make transaction");
+        });
 
-    alert(
-      `amount of ${this.state.balance} transferred from ${this.state.selectFrom} to ${this.state.selectTo}`
-    );
-    event.preventDefault();
+      alert(
+        `Transaction Successful,
+        amount of Rs. ${this.state.balance} transferred from ${this.state.selectFrom} to ${this.state.selectTo}`
+      );
+      event.preventDefault();
+    }
   };
 
   render() {
-
-
-    let button=null;
-    if(this.state.selectFrom && this.state.selectTo && this.state.balance){
-      button = 
-        (
-        <Button type="submit" className="my-3" style={{width:"125px"}}>
+    let button = null;
+    if (this.state.selectFrom && this.state.selectTo && this.state.balance) {
+      button = (
+        <Button type="submit" className="my-3" style={{ width: "125px" }}>
           Submit
         </Button>
-      )
+      );
     }
 
     return (
-      <div className="Form" >
+      <div className="Form">
         <h1>Transaction Form</h1>
-      <Form inline onSubmit={this.handleSubmit} className="Card">
-        <Form.Label className="my-1 mr-3" htmlFor="inlineFormFrom">
-          From
-        </Form.Label>
-        <Form.Control
-          as="select"
-          className="my-1 mr-md-3"
-          id="inlineFormFrom"
-          custom
-          onChange={this.handleSelectFromChange}
-          style={{width:"152px"}}
-        >
-          <option value=""></option>
-          {this.state.allcustomers.map((val, key) => {
-            return (
-              <option value={val.name} key={key}>
-                {val.name}
-              </option>
-            );
-          })}
-        </Form.Control>
-
-        <Form.Label className="my-1 mr-3" htmlFor="inlineFormTo">
-          To
-        </Form.Label>
-        <Form.Control
-          as="select"
-          className="my-1 mr-sm-3"
-          id="inlineFormTo"
-          custom
-          onChange={this.handleSelectToChange}
-          style={{width:"153px"}}
-        >
-          <option value=""></option>
-          {this.state.allcustomers.map((val, key) => {
-            return (
-              <option value={val.name} key={key}>
-                {val.name}
-              </option>
-            );
-          })}
-        </Form.Control>
-
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text>Amount</InputGroup.Text>
-          </InputGroup.Prepend>
+        <Form inline onSubmit={this.handleSubmit} className="Card">
+          <Form.Label className="my-1 mr-3" htmlFor="inlineFormFrom">
+            From
+          </Form.Label>
           <Form.Control
-            type="text"
-            value={this.state.balance}
-            onChange={this.handleBalanceChange}
-          />
-        </InputGroup>
+            as="select"
+            className="my-1 mr-md-3"
+            id="inlineFormFrom"
+            custom
+            onChange={this.handleSelectFromChange}
+            style={{ width: "152px" }}
+          >
+            <option value=""></option>
+            {this.state.allcustomers.map((val, key) => {
+              return (
+                <option value={val.name} key={key}>
+                  {val.name}
+                </option>
+              );
+            })}
+          </Form.Control>
 
-        {button}
-      </Form>
+          <Form.Label className="my-1 mr-3" htmlFor="inlineFormTo">
+            To
+          </Form.Label>
+          <Form.Control
+            as="select"
+            className="my-1 mr-sm-3"
+            id="inlineFormTo"
+            custom
+            onChange={this.handleSelectToChange}
+            style={{ width: "153px" }}
+          >
+            <option value=""></option>
+            {this.state.allcustomers.map((val, key) => {
+              return (
+                <option value={val.name} key={key}>
+                  {val.name}
+                </option>
+              );
+            })}
+          </Form.Control>
+
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>Amount</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control
+              type="text"
+              value={this.state.balance}
+              onChange={this.handleBalanceChange}
+            />
+          </InputGroup>
+
+          {button}
+        </Form>
       </div>
     );
   }
